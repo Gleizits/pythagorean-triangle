ca465e1d8c759ed52d0537a7c7a8c7910f27f72c
Gleizits <geisselespino05@protonmail.com>
2022-06-02 17:21:00 -0500

    Change the color of the console

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    system("color F");
+    cout<<"Pythagorean triangle"<<endl<<endl;
+    float a,b,c;
+    cout<<"Enter the first leg:"<<endl;
+    cin>>a;
+    cout<<"Enter the second leg:"<<endl;
+    cin>>b;
+    c=sqrt(pow(a,2)+pow(b,2));
+    cout<<"The hypotenuse is:"<<"\n"<<c<<endl<<endl;
+    cout<<"\t"<<"_\\"<<endl;
+    cout<<"\t"<<"___\\"<<endl;
+    cout<<"\t"<<"_____\\"<<endl;
+    cout<<"\t"<<"_______\\"<<endl;
+    cout<<"\t"<<"_________\\"<<endl;
+    cout<<"\t"<<a<<"__________"<<c<<endl;
+    cout<<"\t"<<"_____________\\"<<endl;
+    cout<<"\t"<<"_______________\\"<<endl;
+    cout<<"\t"<<"_________________\\"<<endl;
+    cout<<"\t"<<"_________"<<b<<"__________\\"<<endl<<endl;
+    cin.ignore();
+    getch();
+    return 0;
+}
